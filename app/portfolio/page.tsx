dfcc4e6972476f2d89be82a4c12ed6ac8eee7efb
Cameron “Camo” De Vries <141644264+Camo67@users.noreply.github.com>
2025-09-23 20:47:44 +0200

    Delete app directory
    Signed-off-by:  Cameron “Camo” De Vries <141644264+Camo67@users.noreply.github.com>

--- app/portfolio/page.tsx
+++
@@ -1,7 +0,0 @@
-import ClientPortfolio from './ClientPortfolio'
-import { getPortfolioImagesGrouped } from '@/lib/portfolio-utils.server'
-
-export default function PortfolioPage() {
-  const portfolioData = getPortfolioImagesGrouped() // server-side
-  return <ClientPortfolio initialData={portfolioData} />
-}
